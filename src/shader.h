9de5c39ec75ed6fc3da5e22fd55dddeebdeac399
bisthebis <bism25@hotmail.com>
2017-12-05 22:10:15 +0100

    Shader documentation

--- src/shader.h
+++ src/shader.h
@@ -28,19 +28,88 @@ SOFTWARE.
 #include <memory>
 #include <glad/glad.h>
 
+/**
+ * @brief Class holding an OpenGL Shader (one vertex + one fragment)
+ * @invariant : getProgramId() is a valid OpenGL Program
+ * @see Related functions : ShaderUtility
+ *
+ * This class stores an OpenGL Shader Program and manages its lifetime. It is best to use with smart pointers.
+ * Use makeShaderFromFile() and makeShaderFromSource() (in ShaderUtility module) to create a simple shader.
+ *
+ * \code
+ * //Typical usage
+ * string vertexSource, fragmentSource;
+ * // [...]
+ * auto shaderFromFile = makeShaderFromFile("myshader.vert", "myshader.frag");
+ * auto shaderFromSource = makeShaderFromFile(vertexSource, fragmentSource);
+ * //These are of std::unique_ptr<Shader> type.
+ * \endcode
+ */
 class Shader
 {
 public:
+    /**
+     * @brief Builds a "classic" shader (one vertex + one fragment) from source strings
+     * @param vertex : Source code of the Vertex Shader
+     * @param frag : Source code of the Fragment Shader
+     */
     Shader(const std::string& vertex, const std::string& frag);
     ~Shader();
+
+    /**
+     * @brief getProgramId : Returns the Program ID of the underlying shader. Calling glDeleteProgram on it is undefined behavior
+     * @return ProgramID
+     */
     unsigned int getProgramId() const {return program;}
 private:
+    Shader(const Shader&) = delete;
+    Shader& operator= (const Shader&) = delete;
+    Shader(Shader&&) = delete;
+    Shader& operator = (Shader&&) = delete; //Use unique ptrs !
     unsigned int program;
 };
 
+/** @defgroup ShaderUtility
+ * @{ */
+
+/**
+ * @brief compileShader : takes an input string and returns an OpenGL Shader part
+ * @param type : type of the Shader (GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, ...)
+ * @param source : source code
+ * @return the id of the shader.
+ */
+
 unsigned int compileShader(GLenum type, const std::string& source);
+
+/**
+ * @brief readFile : read a file and returns all its content as a std::string
+ * @param path : path of the file
+ * @return string containing file's content
+ * @throw std::runtime_error
+ */
+
 std::string readFile(const std::string& path);
+
+/**
+ * @brief makeShaderFromFile : creates a Shader from two files (by reading them)
+ * @param vertexPath : path of the file containing Vertex Shader source code
+ * @param fragmentPath : path of the file containing Fragment Shader source code.
+ * @return a unique_ptr containing a Shader
+ * @throw std::runtime_error
+ */
+
 std::unique_ptr<Shader> makeShaderFromFile(const std::string& vertexPath, const std::string& fragmentPath);
+
+/**
+ * @brief makeShaderFromFile : creates a Shader from two files (by reading them)
+ * @param vertexPath : Vertex Shader source code
+ * @param fragmentPath :Fragment Shader source code.
+ * @return a unique_ptr containing a Shader
+ * @throw std::runtime_error
+ */
+
 std::unique_ptr<Shader> makeShaderFromSource(const std::string& vertexCode, const std::string& fragmentCode);
+
+/** @} */
 
 #endif // SHADER_H
